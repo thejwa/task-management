dcc25489024fc5ea887178549d8d2dce0a599363
utkirbek <utkirbek_qutfiddinov@mail.ru>
2022-02-24 17:51:07 +0500

    generic dto

--- src/main/java/team/bahor/dto/GenericDto.java
+++ src/main/java/team/bahor/dto/GenericDto.java
@@ -2,11 +2,8 @@ package team.bahor.dto;
 
 import lombok.*;
 
-@Setter
-@Getter
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
 public abstract class GenericDto implements BaseGenericDto {
         private Long id;
 }
--- src/main/java/team/bahor/dto/project/ProjectUpdateDto.java
+++ src/main/java/team/bahor/dto/project/ProjectUpdateDto.java
@@ -1,5 +1,6 @@
 package team.bahor.dto.project;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import team.bahor.dto.GenericDto;
