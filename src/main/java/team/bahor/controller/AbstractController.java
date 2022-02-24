388027178dc745e4a7970b398dc35ace98af063e
dilshodbek99akhmedov <dilshooodbek99akhmedov76@gmail.com>
2022-02-24 17:49:13 +0500

    column page qoshildim v1

--- src/main/java/team/bahor/controller/AbstractController.java
+++ src/main/java/team/bahor/controller/AbstractController.java
@@ -5,6 +5,7 @@ import team.bahor.service.base.BaseGenericService;
 public abstract class AbstractController<S extends BaseGenericService> {
     protected final S service;
 
+
     public AbstractController(S service) {
         this.service = service;
     }
