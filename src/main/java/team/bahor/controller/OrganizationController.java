5455739c1146e1a10532fe274126497d42b5e291
utkirbek <utkirbek_qutfiddinov@mail.ru>
2022-02-24 16:06:26 +0500

    html files updated

--- src/main/java/team/bahor/controller/OrganizationController.java
+++ src/main/java/team/bahor/controller/OrganizationController.java
@@ -1,16 +1,20 @@
 package team.bahor.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
     @RequestMapping("/organization/*")
 public class OrganizationController {
 
-    @RequestMapping("/create")
-    public String homePAge() {
-        return "redirect:/templates/createOrg.html";
+    @RequestMapping("create")
+    public String createOrg() {
+        return "/org/createOrg.html";
+    }
+
+    @RequestMapping("update")
+    public String updateOrg(){
+        return "/org/updateOrg.html";
     }
 
 }
--- src/main/java/team/bahor/controller/ProjectController.java
+++ src/main/java/team/bahor/controller/ProjectController.java
@@ -1,8 +1,18 @@
 package team.bahor.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/project/*")
 public class ProjectController {
+    @RequestMapping("create")
+    public String createProject() {
+        return "/project/createProject.html";
+    }
 
+    @RequestMapping("update")
+    public String updateProject(){
+        return "/project/updateProject.html";
+    }
 }
