07faa4d66d654aa08a5d1314a4f22bca295a05f0
utkirbek <utkirbek_qutfiddinov@mail.ru>
2022-02-25 18:00:33 +0500

    project create added

--- src/main/java/team/bahor/controller/HomeController.java
+++ src/main/java/team/bahor/controller/HomeController.java
@@ -1,5 +1,6 @@
 package team.bahor.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import team.bahor.config.security.UserDetails;
