68752cf4c271dceb6543715e2cbcf859bc404d83
Jonibek WA <jonee.av@gmail.com>
2022-02-28 05:25:30 +0500

    security modified

--- src/main/java/team/bahor/service/comment/CommentServiceImp.java
+++ src/main/java/team/bahor/service/comment/CommentServiceImp.java
@@ -1,5 +1,6 @@
 package team.bahor.service.comment;
 
+import org.springframework.stereotype.Service;
 import team.bahor.dto.comment.CommentCreateDto;
 import team.bahor.dto.comment.CommentDto;
 import team.bahor.dto.comment.CommentUpdateDto;
@@ -10,6 +11,7 @@ import team.bahor.validator.CommentValidator;
 
 import java.util.List;
 
+@Service
 public class CommentServiceImp extends AbstractService<CommentRepository, CommentMapper, CommentValidator> implements CommentService {
     public CommentServiceImp(CommentMapper mapper, CommentValidator validator, CommentRepository repository) {
         super(mapper, validator, repository);
@@ -40,7 +42,7 @@ public class CommentServiceImp extends AbstractService<CommentRepository, Commen
         return null;
     }
 
-    public List<CommentDto> getAllForTask(Long id){
+    public List<CommentDto> getAllForTask(Long id) {
         return mapper.toDto(repository.getByIdForTask(id));
     }
 }
